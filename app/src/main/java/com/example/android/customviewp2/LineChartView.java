bb5394aa8b9c57d292536ec01370210dd77e086c
Pavel Bobkov <microsmaster@yandex.ru>
2015-09-05 16:44:45 +0300

    custom view with animation

--- app/src/main/java/com/example/android/customviewp2/LineChartView.java
+++ app/src/main/java/com/example/android/customviewp2/LineChartView.java
@@ -7,7 +7,9 @@ import android.graphics.Paint;
 import android.graphics.Path;
 import android.util.AttributeSet;
 import android.util.FloatMath;
+import android.util.Log;
 import android.view.View;
+import android.view.animation.AnimationUtils;
 
 /**
  * Created by Pashgan on 05.03.2015.
@@ -17,12 +19,47 @@ public class LineChartView extends View {
     private static final int MIN_LINES = 4;
     private static final int MAX_LINES = 7;
     private static final int[] DISTANCES = { 1, 2, 5 };
+    private static final float GRAPH_SMOOTHNES = 0.15f;
 
-    private float[] datapoints = new float[] {};
+    private Dynamics[] datapoints;
     private Paint paint = new Paint();
 
-    public void setChartData(float[] datapoints) {
-        this.datapoints = datapoints.clone();
+    private Runnable animator = new Runnable() {
+        @Override
+        public void run() {
+            boolean needNewFrame = false;
+            long now = AnimationUtils.currentAnimationTimeMillis();
+            for (Dynamics dynamics : datapoints) {
+                dynamics.update(now);
+                if (!dynamics.isAtRest()) {
+                    needNewFrame = true;
+                }
+            }
+            if (needNewFrame) {
+                postDelayed(this, 15);
+            }
+            invalidate();
+        }
+    };
+
+    public void setChartData(float[] newDatapoints) {
+        long now = AnimationUtils.currentAnimationTimeMillis();
+        if (datapoints == null || datapoints.length != newDatapoints.length) {
+            datapoints = new Dynamics[newDatapoints.length];
+            for (int i = 0; i < newDatapoints.length; i++) {
+                datapoints[i] = new Dynamics(70f, 0.50f);
+                datapoints[i].setPosition(newDatapoints[i], now);
+                datapoints[i].setTargetPosition(newDatapoints[i], now);
+            }
+            invalidate();
+        } else {
+            for (int i = 0; i < newDatapoints.length; i++) {
+                datapoints[i].setTargetPosition(newDatapoints[i], now);
+            }
+            removeCallbacks(animator);
+            post(animator);
+        }
+
     }
 
     public LineChartView(Context context, AttributeSet attrs) {
@@ -31,19 +68,28 @@ public class LineChartView extends View {
 
     @Override
     protected void onDraw(Canvas canvas) {
-        drawBackground(canvas);
-        drawLineChart(canvas);
+        float maxValue = getMax(datapoints);
+        drawBackground(canvas, maxValue);
+        drawLineChart(canvas, maxValue);
     }
 
-    private void drawBackground(Canvas canvas) {
-        float maxValue = getMax(datapoints);
+    private void drawBackground(Canvas canvas, float maxValue) {
         int range = getLineDistance(maxValue);
-
-        paint.setStyle(Paint.Style.STROKE);
+        paint.setStyle(Paint.Style.FILL);
         paint.setColor(Color.GRAY);
+        paint.setTextAlign(Paint.Align.LEFT);
+        paint.setTextSize(16);
+        paint.setStrokeWidth(1);
         for (int y = 0; y < maxValue; y += range) {
-            final float yPos = getYPos(y);
+            final int yPos = (int) getYPos(y, maxValue);
+
+            // turn off anti alias for lines, they get crisper then
+            paint.setAntiAlias(false);
             canvas.drawLine(0, yPos, getWidth(), yPos, paint);
+
+            // turn on anti alias again for the text
+            paint.setAntiAlias(true);
+            canvas.drawText(String.valueOf(y), getPaddingLeft(), yPos - 2, paint);
         }
     }
 
@@ -67,36 +113,64 @@ public class LineChartView extends View {
         return distance;
     }
 
-    private void drawLineChart(Canvas canvas) {
-        Path path = new Path();
-        path.moveTo(getXPos(0), getYPos(datapoints[0]));
+    private void drawLineChart(Canvas canvas, float maxValue) {
+        //код из второй части
+        /*Path path = new Path();
+        path.moveTo(getXPos(0), getYPos(datapoints[0].getPosition(), maxValue));
         for (int i = 1; i < datapoints.length; i++) {
-            path.lineTo(getXPos(i), getYPos(datapoints[i]));
-        }
+            path.lineTo(getXPos(i), getYPos(datapoints[i].getPosition(), maxValue));
+        }*/
+
+        //сглаженный вариант графика
+        Path path = createSmoothPath(maxValue);
 
         paint.setStyle(Paint.Style.STROKE);
         paint.setStrokeWidth(4);
         paint.setColor(0xFF33B5E5);
         paint.setAntiAlias(true);
-        paint.setShadowLayer(4, 2, 2, 0x80000000);
+        paint.setShadowLayer(4, 2, 2, 0x81000000);
         canvas.drawPath(path, paint);
         paint.setShadowLayer(0, 0, 0, 0);
     }
 
+    private Path createSmoothPath(float maxValue) {
 
+        Path path = new Path();
+        path.moveTo(getXPos(0), getYPos(datapoints[0].getPosition(), maxValue));
+        for (int i = 0; i < datapoints.length - 1; i++) {
+            float thisPointX = getXPos(i);
+            float thisPointY = getYPos(datapoints[i].getPosition(), maxValue);
+            float nextPointX = getXPos(i + 1);
+            float nextPointY = getYPos(datapoints[si(i + 1)].getPosition(), maxValue);
 
-    private float getYPos(float value) {
+            float startdiffX = (nextPointX - getXPos(si(i - 1)));
+            float startdiffY = (nextPointY - getYPos(datapoints[si(i - 1)].getPosition(), maxValue));
+            float endDiffX = (getXPos(si(i + 2)) - thisPointX);
+            float endDiffY = (getYPos(datapoints[si(i + 2)].getPosition(), maxValue) - thisPointY);
+
+            float firstControlX = thisPointX + (GRAPH_SMOOTHNES * startdiffX);
+            float firstControlY = thisPointY + (GRAPH_SMOOTHNES * startdiffY);
+            float secondControlX = nextPointX - (GRAPH_SMOOTHNES * endDiffX);
+            float secondControlY = nextPointY - (GRAPH_SMOOTHNES * endDiffY);
+
+            path.cubicTo(firstControlX, firstControlY, secondControlX, secondControlY, nextPointX,
+                    nextPointY);
+        }
+        return path;
+    }
+
+    private float getYPos(float value, float maxValue) {
         float height = getHeight() - getPaddingTop() - getPaddingBottom();
-        float maxValue = getMax(datapoints);
 
-        // масштабирования под высоту view
+        // scale it to the view size
         value = (value / maxValue) * height;
 
-        // инверсия
+        // invert it so that higher values have lower y
         value = height - value;
 
-        // смещение чтобы учесть padding
+        // offset it to adjust for padding
         value += getPaddingTop();
+
         return value;
     }
 
@@ -113,14 +187,23 @@ public class LineChartView extends View {
         return value;
     }
 
-    private float getMax(float[] array) {
-        float max = array[0];
+    private float getMax(Dynamics[] array) {
+        float max = array[0].getPosition();
         for (int i = 1; i < array.length; i++) {
-            if (array[i] > max) {
-                max = array[i];
+            if (array[i].getPosition() > max) {
+                max = array[i].getPosition();
             }
         }
         return max;
+    }
+
+    private int si(int i) {
+        if (i > datapoints.length - 1) {
+            return datapoints.length - 1;
+        } else if (i < 0) {
+            return 0;
+        }
+        return i;
     }
 
 }
--- app/src/main/java/com/example/android/customviewp2/MainActivity.java
+++ app/src/main/java/com/example/android/customviewp2/MainActivity.java
@@ -4,17 +4,37 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends ActionBarActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        LineChartView lineChart = (LineChartView) findViewById(R.id.linechart);
-        lineChart.setChartData(getRandomData());
+        final LineChartView lineChart = (LineChartView) findViewById(R.id.linechart);
+        lineChart.setChartData(getWalkingData());
+
+        findViewById(R.id.walking_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                lineChart.setChartData(getWalkingData());
+            }
+        });
+        findViewById(R.id.running_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                lineChart.setChartData(getRuningData());
+            }
+        });
+        findViewById(R.id.cycling_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                lineChart.setChartData(getCyclingData());
+            }
+        });
     }
 
     @Override
@@ -39,7 +59,15 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private float[] getRandomData() {
+    private float[] getWalkingData() {
         return new float[] { 10, 12, 7, 14, 15, 19, 13, 2, 10, 13, 13, 10, 15, 14 };
+    }
+
+    private float[] getRuningData() {
+        return new float[] { 22, 14, 20, 25, 32, 27, 26, 21, 19, 26, 24, 30, 29, 19 };
+    }
+
+    private float[] getCyclingData() {
+        return new float[] { 0, 0, 0, 10, 14, 23, 40, 35, 32, 37, 41, 32, 18, 39 };
     }
 }
